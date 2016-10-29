46afa30c3931bfd483507b1464d15ecbf160e2a1
nsemmler <ndsunc@gmail.com>
2016-10-29 12:56:05 -0400

    error message if search for artist that does not exist in Spotify API

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -33,20 +33,22 @@ class Artist
 
   def search_and_save_artist(clean_artist, redis)
     data = get_json("https://api.spotify.com/v1/search?type=artist&q=#{clean_artist}")['artists']['items'][0]
-    @name = data['name']
-    @image = data['images'][0]['url']
-    @followers = data['followers']['total']
-    @genres = data['genres']
+    if !data.nil?
+      @name = data['name']
+      @image = data['images'][0]['url']
+      @followers = data['followers']['total']
+      @genres = data['genres']
 
-    gif_data = get_json("http://api.giphy.com/v1/gifs/search?q=#{clean_artist}&api_key=dc6zaTOxFJmzC")['data']
-    @gifs = [
-      gif_data[0]['embed_url'],
-      gif_data[1]['embed_url'],
-      gif_data[2]['embed_url'],
-      gif_data[3]['embed_url']
-    ]
+      gif_data = get_json("http://api.giphy.com/v1/gifs/search?q=#{clean_artist}&api_key=dc6zaTOxFJmzC")['data']
+      @gifs = [
+        gif_data[0]['embed_url'],
+        gif_data[1]['embed_url'],
+        gif_data[2]['embed_url'],
+        gif_data[3]['embed_url']
+      ]
 
-    save(redis)
+      save(redis)
+    end
   end
 
   def save(redis)
