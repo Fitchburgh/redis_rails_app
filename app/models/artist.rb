4c06ebf064078cb726d44642ee60ed83a15343d6
nsemmler <ndsunc@gmail.com>
2016-10-29 13:06:36 -0400

    allows user to search for artists of name length 2 or more

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -32,23 +32,29 @@ class Artist
   end
 
   def search_and_save_artist(clean_artist, redis)
-    data = get_json("https://api.spotify.com/v1/search?type=artist&q=#{clean_artist}")['artists']['items'][0]
-    if !data.nil?
-      @name = data['name']
-      @image = data['images'][0]['url']
-      @followers = data['followers']['total']
-      @genres = data['genres']
-
-      gif_data = get_json("http://api.giphy.com/v1/gifs/search?q=#{clean_artist}&api_key=dc6zaTOxFJmzC")['data']
-      @gifs = [
-        gif_data[0]['embed_url'],
-        gif_data[1]['embed_url'],
-        gif_data[2]['embed_url'],
-        gif_data[3]['embed_url']
-      ]
-
-      save(redis)
+    if clean_artist.length >= 2
+      data = get_json("https://api.spotify.com/v1/search?type=artist&q=#{clean_artist}")['artists']['items'][0]
+      if !data.nil?
+        retrieve_api_information(clean_artist, redis, data)
+      end
     end
+  end
+
+  def retrieve_api_information(clean_artist, redis, data)
+    @name = data['name']
+    @image = data['images'][0]['url']
+    @followers = data['followers']['total']
+    @genres = data['genres']
+
+    gif_data = get_json("http://api.giphy.com/v1/gifs/search?q=#{clean_artist}&api_key=dc6zaTOxFJmzC")['data']
+    @gifs = [
+      gif_data[0]['embed_url'],
+      gif_data[1]['embed_url'],
+      gif_data[2]['embed_url'],
+      gif_data[3]['embed_url']
+    ]
+
+    save(redis)
   end
 
   def save(redis)
