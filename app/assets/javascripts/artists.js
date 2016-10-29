7f20fab60e123555069b81cb4f18437d4f8314f1
nsemmler <ndsunc@gmail.com>
2016-10-29 13:58:47 -0400

    neon light header effect

--- app/assets/javascripts/artists.js
+++ app/assets/javascripts/artists.js
@@ -1,17 +1,8 @@
-// $(document).ready(function () {
-//   $("a.genre").colorbox({
-//     rel: 'genre',
-//     dataType: 'jsonp',
-//     transition: "elastic",
-//     width: "75%",
-//     height: "75%",
-//     previous: "<",
-//     next: ">",
-//   })
-// })
+
 $('body').animate({
   scrollTop: target.offset().top
 }, 1000);
+
 
 $('.container-all').fadeOut('slow', function(){
     $('.container-all').load(link+' #content', function(){
