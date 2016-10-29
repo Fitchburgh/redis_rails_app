0c311b61ab11cd4b0a334f5f04264183975434bd
nsemmler <ndsunc@gmail.com>
2016-10-29 09:13:17 -0400

    changed whitespace

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -12,49 +12,29 @@ class Artist
 
     clean_artist = @artist.downcase.gsub(/\s+/, "+")
 
-    if not_have_artist(clean_artist)
-      search_and_save_artist(clean_artist)
+    if not_have_artist(clean_artist, redis)
+      search_and_save_artist(clean_artist, redis)
     else
-      find_artist(clean_artist)
+      find_artist(clean_artist, redis)
     end
   end
 
-  def save
-    redis = Redis.new
-
-    clean_artist = @artist.downcase.gsub(/\s+/, "+")
-
-    redis.set(clean_artist + ':artist', @name)
-
-    redis.set(clean_artist + ':followers', @followers)
-
-    redis.set(clean_artist + ':genres', JSON.dump(@genres))
-
-    redis.set(clean_artist + ':gifs', JSON.dump(@gifs))
-
-    redis.set(clean_artist + ':image', @image)
-  end
-
-  def get_json(url)
-    HTTParty.get(url).parsed_response
-  end
-
-  def not_have_artist(clean_artist)
-    redis = Redis.new
+  def not_have_artist(clean_artist, redis)
     redis.get(clean_artist + ':artist').nil?
   end
 
-  def find_artist(clean_artist)
+  def find_artist(clean_artist, redis)
     @name = redis.get(clean_artist + ':artist')
+    @image = redis.get(clean_artist + ':image')
     @followers = redis.get(clean_artist + ':followers')
     @genres = JSON.parse(redis.get(clean_artist + ':genres'))
     @gifs = JSON.parse(redis.get(clean_artist + ':gifs'))
-    @image = redis.get(clean_artist + ':image')
   end
 
-  def search_and_save_artist(clean_artist)
+  def search_and_save_artist(clean_artist, redis)
     data = get_json("https://api.spotify.com/v1/search?type=artist&q=#{clean_artist}")['artists']['items'][0]
     @name = data['name']
+    @image = data['images'][0]['url']
     @followers = data['followers']['total']
     @genres = data['genres']
 
@@ -66,8 +46,20 @@ class Artist
       gif_data[3]['embed_url']
     ]
 
-    @image = data['images'][0]['url']
+    save(redis)
+  end
 
-    save
+  def save(redis)
+    clean_artist = @artist.downcase.gsub(/\s+/, "+")
+
+    redis.set(clean_artist + ':artist', @name)
+    redis.set(clean_artist + ':image', @image)
+    redis.set(clean_artist + ':followers', @followers)
+    redis.set(clean_artist + ':genres', JSON.dump(@genres))
+    redis.set(clean_artist + ':gifs', JSON.dump(@gifs))
+  end
+
+  def get_json(url)
+    HTTParty.get(url).parsed_response
   end
 end
