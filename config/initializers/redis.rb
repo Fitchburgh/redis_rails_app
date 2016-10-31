8ab3ff28760cd681a1ce307a39fef007c4dbdca4
nsemmler <ndsunc@gmail.com>
2016-10-31 15:53:20 -0400

    changed Redis.current_URL to REDIS_URL

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 require 'redis'
 
 # Redis.current = Redis.new(:host => 'redis-animate-53848')
-uri = URI.parse(ENV["Redis.current_URL"])
+uri = URI.parse(ENV["REDIS_URL"])
 Redis.current = Redis.new(:url => uri)
