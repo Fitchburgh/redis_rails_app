bfcd925eff82fa4a2446097ae0ab59b1ae6c9601
nsemmler <ndsunc@gmail.com>
2016-10-31 14:30:13 -0400

    adding heroku redis stuff for heroku created redis server

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,5 @@
 require 'redis'
 
-Redis.current = Redis.new(:host => 'redis-animate-53848')
+# Redis.current = Redis.new(:host => 'redis-animate-53848')
+uri = URI.parse(ENV["REDIS_URL"])
+Redis.current = Redis.new(:url => uri)
