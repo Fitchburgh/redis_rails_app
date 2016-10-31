a67bc207eb74079afb4f55eb8ef1d164ef59a340
nsemmler <ndsunc@gmail.com>
2016-10-31 14:19:45 -0400

    changed redis host from local to heroku generated redis url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
 require 'redis'
 
-Redis.current = Redis.new(:host => 'redis://h:peouqndvghr88kcpk7h64u4194p@ec2-54-225-80-250.compute-1.amazonaws.com:10279')
+Redis.current = Redis.new(:host => 'redis-animate-53848')
