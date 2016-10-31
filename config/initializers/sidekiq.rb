52ccd1d1be822d35015260fc915a500d29008d6e
nsemmler <ndsunc@gmail.com>
2016-10-31 15:09:33 -0400

    edited heroku generated redis url

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://h:peouqndvghr88kcpk7h64u4194p@ec2-54-225-80-250.compute-1.amazonaws.com:10279' }
+  config.redis = { url: 'redis://ec2-54-225-80-250.compute-1.amazonaws.com:10279' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://h:peouqndvghr88kcpk7h64u4194p@ec2-54-225-80-250.compute-1.amazonaws.com:10279' }
+  config.redis = { url: 'redis://ec2-54-225-80-250.compute-1.amazonaws.com:10279' }
 end
