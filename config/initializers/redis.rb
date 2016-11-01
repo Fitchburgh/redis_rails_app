93bdfa723540a97d00a3f8700237bb4aeea2662e
nsemmler <ndsunc@gmail.com>
2016-11-01 12:08:28 -0400

    changing localhost to heroku created redis server

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,9 @@
 require 'redis'
 
 # Redis.current = Redis.new(:host => 'redis-animate-53848')
-# uri = URI.parse(ENV["REDIS_URL"])
-# Redis.current = Redis.new(:url => uri)
-
-Redis.current = Redis.new(:host => 'localhost', :port => 6379)
+if Rails.env.production?
+  uri = URI.parse(ENV["REDIS_URL"])
+  Redis.current = Redis.new(:url => uri)
+else
+  Redis.current = Redis.new(:host => 'localhost', :port => 6379)
+end
