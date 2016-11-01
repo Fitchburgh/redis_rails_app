10e6ba1ecca6db516aa45f5a2dd12141c4f369e2
nsemmler <ndsunc@gmail.com>
2016-11-01 12:54:04 -0400

    removed http: from giphy API url

--- app/workers/gif_set_worker.rb
+++ app/workers/gif_set_worker.rb
@@ -7,7 +7,7 @@ class GifSetWorker
   include Sidekiq::Worker
   sidekiq_options :retry => false
   def perform(clean_artist)
-    @gif_data = get_json("http://api.giphy.com/v1/gifs/search?q=#{clean_artist}&api_key=dc6zaTOxFJmzC")['data']
+    @gif_data = get_json("//api.giphy.com/v1/gifs/search?q=#{clean_artist}&api_key=dc6zaTOxFJmzC")['data']
 
     gifs = [
       @gif_data[0]['embed_url'],
